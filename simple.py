df22cc346a1ee549f7ea0cfd17323282c07d3cce
Jan Bodnar <jan.bodnar@gmail.com>
2018-10-11 11:37:40 +0200

    Create simple.py

---
+++ simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+win = Gtk.Window()
+win.connect("destroy", Gtk.main_quit)
+win.show()
+Gtk.main()
